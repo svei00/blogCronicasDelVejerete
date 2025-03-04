3cfdbecceea5d28a5bf0b9a44553a85e06b78da2
svei00 <79055634+svei00@users.noreply.github.com>
2025-03-03 20:21:20 -0800

    Create the header section

--- next-blog/src/app/page.tsx
+++ next-blog/src/app/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function page() {
-  return <div>Cro{"\u0301"}nicas del Vejerete</div>;
+  return (
+    <div className="font-semibold text-gray-200 bg-purple-600 dark:text-gray-200 dark:bg-purple-400">
+      Cro{"\u0301"}nicas del Vejerete
+    </div>
+  );
 }
